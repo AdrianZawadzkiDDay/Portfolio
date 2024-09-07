308c030bb0b83ee2c1ea168c8b320f598d553272
Adrian Zawadzki <zawadzkii@tlen.pl>
2024-09-07 16:49:03 +0200

    experience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import aboutImg from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../constants";
 const About = () => {
   return (
-    <div className="border-b border-neutra-900 pb-4">
+    <div className="border-b border-neutral-900 pb-4">
         <h1 className="my-20 text-center text-4xl">
            About
            <span className="text-neutral-500"> Me</span>
